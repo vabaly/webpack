946f997639a4bfee8566326e463bf53702eab1dd
vabaly <love0and0life@gmail.com>
2020-02-10 08:51:43 +0800

    feat: compiler, watching 等注释

--- hot/dev-server.js
+++ hot/dev-server.js
@@ -1,6 +1,7 @@
 /*
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
+	此 JS 也会被作为一个 entry 打包进客户端的 JS 中
 */
 /* globals __webpack_hash__ */
 if (module.hot) {
@@ -9,7 +10,11 @@ if (module.hot) {
 		return lastHash.indexOf(__webpack_hash__) >= 0;
 	};
 	var log = require("./log");
+	// 实际检查热更新的代码
 	var check = function check() {
+		// 【Todo】module.hot.check 是什么呢？
+		// 目前只知道 module.hot 是 HotModuleReplacementPlugin 添加的
+		// HotModuleReplacementPlugin 源码在 ../lib/HotModuleReplacementPlugin.js
 		module.hot
 			.check(true)
 			.then(function(updatedModules) {
@@ -48,10 +53,12 @@ if (module.hot) {
 			});
 	};
 	var hotEmitter = require("./emitter");
+	// 当客户端 JS 进行热更新时，emit 此事件
 	hotEmitter.on("webpackHotUpdate", function(currentHash) {
 		lastHash = currentHash;
 		if (!upToDate() && module.hot.status() === "idle") {
 			log("info", "[HMR] Checking for updates on the server...");
+			// 实际检查热更新的代码
 			check();
 		}
 	});
